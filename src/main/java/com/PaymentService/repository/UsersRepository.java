56400c236d86cf7c908d457b2c78e25e4029e6c1
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-11-20 14:07:05 +0600

    "Added new Role API class"

--- src/main/java/com/PaymentService/repository/UsersRepository.java
+++ src/main/java/com/PaymentService/repository/UsersRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
     UsersEntity findByUserNameAndStatus(String name, Integer status);
-
+UsersEntity findByIdAndStatus(Long id, Integer status);
     //private final CompanyRepository companyRepo;
 }
