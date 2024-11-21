de39fb3618ee42d008e809a9a4d2d52ea66666be
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-11-21 12:35:57 +0600

    "Added new Role API class"

--- src/main/java/com/PaymentService/utils/UrlConstraint.java
+++ src/main/java/com/PaymentService/utils/UrlConstraint.java
@@ -35,6 +35,7 @@ public final class UrlConstraint {
         public static final String CREATE="/create";
         public static final String GET_ALL_By_UserId="/all";
         public static final String GET_USER_BY_ID="/get/{id}";
+        public static final String DELETE_USER_BY_ID="/delete/{id}";
     }
     public static class Privilege{
         public static final String ROOT="/privilege";
