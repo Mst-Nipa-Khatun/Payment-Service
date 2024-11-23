3c312c71f64eaa03f9f8ff7b559c93d6e3c18bc0
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-11-23 10:29:04 +0600

    "Added new Privilege API class"

--- src/main/java/com/PaymentService/utils/UrlConstraint.java
+++ src/main/java/com/PaymentService/utils/UrlConstraint.java
@@ -42,6 +42,7 @@ public final class UrlConstraint {
         public static final String ROOT="/privilege";
         public static final String CREATE="/create";
         public static final String GET_ALL="/all";
+        public static final String GET_USER_BY_ID="/get/{id}";
     }
 
     }
