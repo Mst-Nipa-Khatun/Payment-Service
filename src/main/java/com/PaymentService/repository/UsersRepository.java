4e9ac5536fe6e6f0bd2f90743966644241a124d4
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-04 11:09:52 +0600

    "Edited new API class"

--- src/main/java/com/PaymentService/repository/UsersRepository.java
+++ src/main/java/com/PaymentService/repository/UsersRepository.java
@@ -12,18 +12,14 @@ public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
     UsersEntity findByUserNameAndStatus(String name, Integer status);
     UsersEntity findByIdAndStatus(Long id, Integer status);//Uses RoleServiceImpl
     List<UsersEntity> findAllByStatus(Integer status);
-     //UsersEntity findById(long id);
-    UsersEntity findByEmailAndPassword(String email, String password);
-    UsersEntity findByIdAndEmailAndPassword(Long id, String email, String password);
-  //  UsersEntity findByUserNameAndVowels(String name, String vowels);
 
-    @Query("select u from UsersEntity u where substring(lower(u.userName),1,1) in ('a','e','i','o','u')")
-    List<UsersEntity> findByUserNameStartingWithVowel();
+//    UsersEntity findByEmailAndPassword(String email, String password);
+//    UsersEntity findByIdAndEmailAndPassword(Long id, String email, String password);
 
+//    @Query("select u from UsersEntity u where substring(lower(u.userName),1,1) in ('a','e','i','o','u')")
+//    List<UsersEntity> findByUserNameStartingWithVowel();
 
 
-    // List<RoleEntity> findByRoleNameStartingWithVowel(); //role Repo te add korte hobe
-   // List<String> findByNamesStartingWithVowelFromUsersAndRoles();
 
 
 
