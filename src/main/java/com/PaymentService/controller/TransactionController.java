c79e8c112f24319ab858581f1c2ceaab769433f3
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-04 15:29:31 +0600

    "Created new Controller which is TransactionController"

--- src/main/java/com/PaymentService/controller/TransactionController.java
+++ src/main/java/com/PaymentService/controller/TransactionController.java
@@ -1,4 +1,26 @@
 package com.PaymentService.controller;
 
+import com.PaymentService.dto.Response;
+import com.PaymentService.dto.TransactionDto;
+import com.PaymentService.service.TransactionService;
+import com.PaymentService.utils.UrlConstraint;
+import jakarta.transaction.Transaction;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(UrlConstraint.Transaction.ROOT)
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @PostMapping(UrlConstraint.Transaction.CREATE)
+    public Response createTransaction(@RequestBody TransactionDto transactionDto) {
+        return transactionService.createTransaction(transactionDto);
+
+    }
 }
