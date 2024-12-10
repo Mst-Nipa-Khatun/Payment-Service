dab93ccadf42f1395e45361fbb5e5dd5ebd1e718
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-10 13:28:56 +0600

    "Added new API in StudentEntity"

--- src/main/java/com/PaymentService/utils/UrlConstraint.java
+++ src/main/java/com/PaymentService/utils/UrlConstraint.java
@@ -13,6 +13,7 @@ public final class UrlConstraint {
         public static final String EDIT_PAYMENT="/edit/{id}";
         public static final String GET_NULL_AGE="/nullAge";
         public static final String GET_AGE_BY_GENDER_DESC="/getAgeByDESC";
+        public static final String GET_STUDENTS_BY_AGE="/getStudentsByAge";
 
     }
     public static class Company {
