4ac05077768743b005d31447e4274ba1f91b01b7
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-11 12:30:13 +0600

    "Added new API in userRepo "

--- src/main/java/com/PaymentService/repository/UsersRepository.java
+++ src/main/java/com/PaymentService/repository/UsersRepository.java
@@ -13,14 +13,12 @@ public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
     UsersEntity findByIdAndStatus(Long id, Integer status);//Uses RoleServiceImpl
     List<UsersEntity> findAllByStatus(Integer status);
 
-//    UsersEntity findByEmailAndPassword(String email, String password);
-//    UsersEntity findByIdAndEmailAndPassword(Long id, String email, String password);
+
 
 //    @Query("select u from UsersEntity u where substring(lower(u.userName),1,1) in ('a','e','i','o','u')")
 //    List<UsersEntity> findByUserNameStartingWithVowel();
 
 
-    List<UsersEntity> findDistinctByUserNameAndFullNameAndStatus(String userName, String fullName, Integer status);
    List<UsersEntity> findDistinctByFullNameAndUserName(String fullName, String userName);
 
    List<UsersEntity> findByUserNameLikeAndStatus(String pattern, Integer status);
