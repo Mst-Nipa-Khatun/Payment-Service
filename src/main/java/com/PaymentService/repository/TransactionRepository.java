5cd1e0c31fdd551093574766eeabd4b2e62240e6
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-11 11:29:17 +0600

    "Added new API in transaction "

--- src/main/java/com/PaymentService/repository/TransactionRepository.java
+++ src/main/java/com/PaymentService/repository/TransactionRepository.java
@@ -4,6 +4,7 @@ import com.PaymentService.entity.TransactionEntity;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,11 +12,27 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
     TransactionEntity findByTransactionIdAndStatus(Long transactionId, Integer status);
-  List<TransactionEntity> findByAmountLessThanEqualAndStatus(Double amount, Integer status);
-  List<TransactionEntity> findAllByStatus(Integer status);
+
+    List<TransactionEntity> findByAmountLessThanEqualAndStatus(Double amount, Integer status);
+
+    List<TransactionEntity> findAllByStatus(Integer status);
 
 
-//select *from Transaction where amount>=500 and amount <=1000 and status = 1;
+
+    @Query(value = "select  *from Transaction where trim(lower(transaction_type))=lower(:transaction_type) AND amount BETWEEN :startAmount" +
+            " AND :endAmount", nativeQuery = true)
+    List<TransactionEntity> getSpecificDataWithNativeQuery(@Param("transaction_type") String transaction_type,
+                                            @Param("startAmount") Double startAmount,
+                                            @Param("endAmount") Double endAmount);
+
+
+    @Query(value = "select  u from TransactionEntity u where trim(lower(u.transactionType))=lower(:transaction_type) AND u.amount BETWEEN :startAmount" +
+            " AND :endAmount")
+    List<TransactionEntity> getSpecificDataWithoutNativeQuery(@Param("transaction_type") String transaction_type,
+                                                              @Param("startAmount") Double startAmount,
+                                                              @Param("endAmount") Double endAmount);
+
+
     List<TransactionEntity> findByAmountGreaterThanEqualAndAmountLessThanEqualAndStatus(Double minAmount,
                                                                                         Double maxAmount
             , Integer status);
