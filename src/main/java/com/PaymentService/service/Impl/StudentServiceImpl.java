31ea1d04d5bbef6fee864c3bac03cb5a813addb6
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-17 11:30:14 +0600

    "Added new API in Company "

--- src/main/java/com/PaymentService/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/PaymentService/service/Impl/StudentServiceImpl.java
@@ -133,7 +133,8 @@ public class StudentServiceImpl implements StudentService {
             return ResponseBuilder.getSuccessResponse(HttpStatus.OK,studentDtos,
                     "Successfully retrieved");
         }
-        return ResponseBuilder.getFailResponse(HttpStatus.NO_CONTENT,null,"Student not found");
+        return ResponseBuilder.getFailResponse(HttpStatus.NO_CONTENT,null,
+                "Student not found");
     }
 
     @Override
