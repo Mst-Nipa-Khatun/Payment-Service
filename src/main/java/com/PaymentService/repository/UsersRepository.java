e9c8c6789060a7ff8e85adb7c4f46491cdaf97b3
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-23 17:59:42 +0600

    "Edited"

--- src/main/java/com/PaymentService/repository/UsersRepository.java
+++ src/main/java/com/PaymentService/repository/UsersRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
-    @Query(value = "SELECT * FROM users WHERE username = :username AND status = :status", nativeQuery = true)
-    UsersEntity findByUserNameAndStatusNative(@Param("userName") String userName, @Param("status") Integer status);
+//    @Query(value = "SELECT * FROM users WHERE username = :username AND status =:status", nativeQuery = true)
+//    UsersEntity findByUserNameAndStatusNative(@Param("userName") String userName, @Param("status") Integer status);
     UsersEntity findByUserNameAndStatus(String userName,Integer status);
-
-    @Query(value = "select *from users where id=:id AND status=:status", nativeQuery = true)
-    UsersEntity findByIdAndStatusNative(@Param("id") Long id,@Param("status") Integer status);
+//
+//    @Query(value = "select *from users where id=:id AND status=:status", nativeQuery = true)
+//    UsersEntity findByIdAndStatusNative(@Param("id") Long id,@Param("status") Integer status);
     UsersEntity findByIdAndStatus(Long id, Integer status);
 
     @Query(value = "SELECT * FROM users WHERE status = :status", nativeQuery = true)
@@ -31,13 +31,13 @@ public interface UsersRepository extends JpaRepository<UsersEntity, Long> {
 //    @Query("select u from UsersEntity u where substring(lower(u.userName),1,1) in ('a','e','i','o','u')")
 //    List<UsersEntity> findByUserNameStartingWithVowel();
 
-@Query(value = "SELECT DISTINCT *from users where fullName=:fullName and userName=:userName And status=:status", nativeQuery = true)
+@Query(value = "SELECT DISTINCT *from users where fullName=:fullName and userName=:userName And status=1", nativeQuery = true)
 List<UsersEntity> findDistinctByFullNameAndUserNameNative(@Param("fullName") String fullName, @Param("userName") String userName);
 
    List<UsersEntity> findDistinctByFullNameAndUserName(String fullName, String userName);
 
-   @Query(value = "SELECT *from users where userName=:userName LIKE 'N%'",nativeQuery=true)
-   List<UsersEntity> findByUserNameLikeAndStatus(String pattern, Integer status);
+   @Query(value = "SELECT *from users where userName=:userName LIKE 'N%' and status =:status",nativeQuery=true)
+   List<UsersEntity> findByUserNameLikeAndStatus(@Param("userName") String pattern, @Param("status") Integer status);
 
 
 }
