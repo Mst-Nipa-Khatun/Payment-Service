c044b57a223ee93995373b1b90678afc5fe02886
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-23 17:59:00 +0600

    "Edited"

---
+++ src/main/java/com/PaymentService/entity/StudentInformationTest.java
@@ -0,0 +1,31 @@
+package com.PaymentService.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "STUDENTINFORMATION")
+public class StudentInformationTest {
+    @Id
+    @Column(name = "Roll")
+    private Long roll;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Gender")
+    private String gender;
+
+    @Column(name = "Age")
+    private Integer age;
+
+    @Column(name = "GPA")
+    private Double gpa;
+
+    @Column(name = "City")
+    private String city;
+}
