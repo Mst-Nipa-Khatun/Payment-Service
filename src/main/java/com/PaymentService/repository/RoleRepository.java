1eddb7df3e247055a40c62556e749f62287ecb32
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2024-12-23 17:58:27 +0600

    "Added my roleRepo feild"

--- src/main/java/com/PaymentService/repository/RoleRepository.java
+++ src/main/java/com/PaymentService/repository/RoleRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
     RoleEntity findByNameAndStatus(String name, Integer status);
     RoleEntity findByIdAndStatus(Long id, Integer status);
-    List<RoleEntity> findAllByStatus(Integer status);
+    //List<RoleEntity> findAllByStatus(Integer status);
 
 //    @Query("select u from RoleEntity u where substring(lower(u.name),1,1) in ('a','e','i','o','u')")
 //    List<RoleEntity> findByRoleNameStartingWithVowel();///eita ekta example
