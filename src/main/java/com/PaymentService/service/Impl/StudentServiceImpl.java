debfa289ff903cabfa1ea7e13bfb0851071aa50d
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-05 11:51:51 +0600

    "Added my README feild"

--- src/main/java/com/PaymentService/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/PaymentService/service/Impl/StudentServiceImpl.java
@@ -77,8 +77,7 @@ public class StudentServiceImpl implements StudentService {
             return ResponseBuilder.getSuccessResponse(HttpStatus.OK,createstudentDto,
                     "Successfully deleted");
         }
-        return ResponseBuilder.getFailResponse(HttpStatus.NO_CONTENT,null,
-                "Student not found");
+        return ResponseBuilder.getFailResponse(HttpStatus.NO_CONTENT,null, "Student not found");
     }
 
     @Override
