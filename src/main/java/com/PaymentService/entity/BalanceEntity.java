2305baf8409b8ec73eb8a3ded689ff643aabf669
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-06 10:33:27 +0600

    "Added New API Balance"

---
+++ src/main/java/com/PaymentService/entity/BalanceEntity.java
@@ -0,0 +1,21 @@
+package com.PaymentService.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class BalanceEntity extends BaseEntity{
+    @Id
+    @Column(name = "Balance")
+    private double balance;
+    @Column(name = "Currency")
+    private String currency;
+    @Column(name = "LioBalance")
+    private String lioBalance;
+    @Column(name = "Account")
+    private String account;
+
+}
