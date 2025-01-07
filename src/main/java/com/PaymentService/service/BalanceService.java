65214e616f752f1d6e751f2fbd08fbf3f4223e3e
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 15:45:19 +0600

    "Added New API Balance"

--- src/main/java/com/PaymentService/service/BalanceService.java
+++ src/main/java/com/PaymentService/service/BalanceService.java
@@ -5,4 +5,8 @@ import com.PaymentService.dto.Response;
 
 public interface BalanceService {
     Response createBalance(BalanceDto balanceDto);
+    Response getAllBalance();
+    Response getBalanceById(Long id);
+    Response deleteBalanceById(Long id);
+    Response editBalanceById(Long id, BalanceDto balanceDto);
 }
