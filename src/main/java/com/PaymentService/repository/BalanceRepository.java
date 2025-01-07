1498662a4f3652d18fa474a52b8d8ac7e2966ffe
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 15:45:07 +0600

    "Added New API Balance"

--- src/main/java/com/PaymentService/repository/BalanceRepository.java
+++ src/main/java/com/PaymentService/repository/BalanceRepository.java
@@ -4,8 +4,12 @@ import com.PaymentService.entity.BalanceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BalanceRepository extends JpaRepository <BalanceEntity,Long> {
-    BalanceEntity findByBalanceAndStatus(Double balance, Integer status);
     BalanceEntity findByUserIdAndStatus(Long userId, Integer status);
+    List<BalanceEntity> findAllByStatus(Integer status);
+    BalanceEntity findByIdAndStatus(Long id, Integer status);
+
 }
