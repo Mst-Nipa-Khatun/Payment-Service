201c785100c9511c49b1b94dfaf93d90c7f056a4
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 12:43:47 +0600

    "Added New API Account"

--- src/main/java/com/PaymentService/service/Impl/AccountServiceImpl.java
+++ src/main/java/com/PaymentService/service/Impl/AccountServiceImpl.java
@@ -10,6 +10,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
@@ -35,5 +38,35 @@ public class AccountServiceImpl implements AccountService {
             return ResponseBuilder.getSuccessResponse(HttpStatus.CREATED,savedAccount,"Created");
         }
         return ResponseBuilder.getFailResponse(HttpStatus.BAD_REQUEST,null,"Account already exists");
+    }
+
+    @Override
+    public Response getAllAccount() {
+        List<AccountEntity> accountEntities=accountRepository.findAllByAndStatus(1);
+        if(!accountEntities.isEmpty()){
+            List<AccountDto> accountDtos=new ArrayList<>();
+            for(AccountEntity accountEntity:accountEntities){
+                AccountDto accountDto=modelMapper.map(accountEntity,AccountDto.class);
+                accountDtos.add(accountDto);
+            }
+            return ResponseBuilder.getSuccessResponse(HttpStatus.OK,accountDtos,"Get All Account");
+        }
+        return ResponseBuilder.getFailResponse(HttpStatus.BAD_REQUEST,null,"No account found");
+    }
+
+    @Override
+    public Response getAccountById(Long id) {
+        AccountEntity account=accountRepository.findByIdAndStatus(id,1);
+        if(account !=null){
+            account.setAccName(""+account.getAccNumber());
+            account.setAccNumber(account.getAccNumber());
+            account.setRegion(account.getRegion());
+            account.setAccType(account.getAccType());
+            AccountEntity accountEntity=accountRepository.save(account);
+
+           // AccountDto accountDto=modelMapper.map(account,AccountDto.class);
+            return ResponseBuilder.getSuccessResponse(HttpStatus.OK,accountEntity,"Get Account");
+        }
+        return ResponseBuilder.getFailResponse(HttpStatus.BAD_REQUEST,null,"No account found");
     }
 }
