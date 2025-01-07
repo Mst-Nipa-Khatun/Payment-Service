8f01be204bdbe1401442e7e7f77b3bdb07ad6f05
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 12:43:15 +0600

    "Added New API Account"

--- src/main/java/com/PaymentService/controller/AccountController.java
+++ src/main/java/com/PaymentService/controller/AccountController.java
@@ -4,10 +4,7 @@ import com.PaymentService.dto.AccountDto;
 import com.PaymentService.dto.Response;
 import com.PaymentService.service.AccountService;
 import com.PaymentService.utils.UrlConstraint;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(UrlConstraint.Account.ROOT)
@@ -21,5 +18,13 @@ public class AccountController {
     @PostMapping(UrlConstraint.Account.CREATE)
     public Response createAccount(@RequestBody AccountDto accountDto) {
         return accountService.createAccount(accountDto);
+    }
+    @GetMapping(UrlConstraint.Account.GET_ALL)
+    public Response getAllAccounts() {
+        return accountService.getAllAccount();
+    }
+    @GetMapping(UrlConstraint.Account.GET_ACCOUNT_BY_ID)
+    public Response getAccountById(@PathVariable("id") long id) {
+        return accountService.getAccountById(id);
     }
 }
