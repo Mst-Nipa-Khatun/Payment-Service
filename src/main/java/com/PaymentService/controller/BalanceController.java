a5c5f9e6750dfddeb4314e12e74a33b07184655c
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 15:44:56 +0600

    "Added New API Balance"

--- src/main/java/com/PaymentService/controller/BalanceController.java
+++ src/main/java/com/PaymentService/controller/BalanceController.java
@@ -4,10 +4,7 @@ import com.PaymentService.dto.BalanceDto;
 import com.PaymentService.dto.Response;
 import com.PaymentService.service.BalanceService;
 import com.PaymentService.utils.UrlConstraint;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(UrlConstraint.Balance.ROOT)
@@ -21,5 +18,21 @@ public class BalanceController {
     @PostMapping(UrlConstraint.Balance.CREATE)
     public Response createBalance(@RequestBody BalanceDto balanceDto) {
     return balanceService.createBalance(balanceDto);
-}
+    }
+    @GetMapping(UrlConstraint.Balance.GET_ALL)
+    public Response getAllBalances() {
+        return balanceService.getAllBalance();
+    }
+    @GetMapping(UrlConstraint.Balance.GET_BALANCE_BY_ID)
+    public Response getBalanceById(@PathVariable("id") Long id) {
+       return balanceService.getBalanceById(id);
+    }
+    @DeleteMapping(UrlConstraint.Balance.DELETE_BALANCE_BY_ID)
+    public Response deleteBalanceById(@PathVariable("id") Long id) {
+        return balanceService.deleteBalanceById(id);
+    }
+    @PutMapping(UrlConstraint.Balance.EDIT_BALANCE_BY_ID)
+    public Response editBalanceById(@PathVariable("id") Long id, @RequestBody BalanceDto balanceDto) {
+        return balanceService.editBalanceById(id,balanceDto);
+    }
 }
