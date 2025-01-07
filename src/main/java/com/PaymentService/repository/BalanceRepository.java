25d605dce659a02dac20068c74c2615ecce54b92
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 10:34:07 +0600

    "Added New API Balance"

--- src/main/java/com/PaymentService/repository/BalanceRepository.java
+++ src/main/java/com/PaymentService/repository/BalanceRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BalanceRepository extends JpaRepository <BalanceEntity,Long> {
     BalanceEntity findByBalanceAndStatus(Double balance, Integer status);
+    BalanceEntity findByUserIdAndStatus(Long userId, Integer status);
 }
