1abe60c5a4294e6cc4c21068300796ac6ee82d37
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-07 12:43:25 +0600

    "Added New API Account"

--- src/main/java/com/PaymentService/repository/AccountRepository.java
+++ src/main/java/com/PaymentService/repository/AccountRepository.java
@@ -4,8 +4,12 @@ import com.PaymentService.entity.AccountEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<AccountEntity,Long> {
 
     AccountEntity findByAccNameAndStatus(String accName,Integer status);
+    List<AccountEntity> findAllByAndStatus(Integer status);
+    AccountEntity findByIdAndStatus(Long id,Integer status);
 }
