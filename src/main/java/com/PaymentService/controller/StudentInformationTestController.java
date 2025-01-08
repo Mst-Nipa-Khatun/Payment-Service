b77a564e3429d714dd019c206a8b20a960aef867
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-08 20:19:41 +0600

    "Added New API User"

--- src/main/java/com/PaymentService/controller/StudentInformationTestController.java
+++ src/main/java/com/PaymentService/controller/StudentInformationTestController.java
@@ -1,20 +1,31 @@
 package com.PaymentService.controller;
 
+import com.PaymentService.dto.UserDetailsProjection;
+import com.PaymentService.repository.BalanceRepository;
 import com.PaymentService.repository.StudentInformationTestRepo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StudentInformationTestController {
 
     private final StudentInformationTestRepo studentInformationTestRepo;
+    private final BalanceRepository balanceRepository;
 
-    public StudentInformationTestController(StudentInformationTestRepo studentInformationTestRepo) {
+    public StudentInformationTestController(StudentInformationTestRepo studentInformationTestRepo, BalanceRepository balanceRepository) {
         this.studentInformationTestRepo = studentInformationTestRepo;
+        this.balanceRepository = balanceRepository;
     }
 
     @GetMapping("/testData")
-    public Object getTestData(){
-        return studentInformationTestRepo.getTestData();
+    public List<Map<String, Object>> getTestData(){
+        return balanceRepository.findByDetails();
+    }
+    @GetMapping("/testData2")
+    public List<UserDetailsProjection> findByDetails2ndApproach(){
+        return balanceRepository.findByDetails2ndApproach();
     }
 }
