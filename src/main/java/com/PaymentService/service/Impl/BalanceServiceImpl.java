0fdb6d73f3f1b8cda6ac1bdcc0d73d0394bd78e1
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-09 17:54:45 +0600

    "Added New API Balance"

--- src/main/java/com/PaymentService/service/Impl/BalanceServiceImpl.java
+++ src/main/java/com/PaymentService/service/Impl/BalanceServiceImpl.java
@@ -91,7 +91,8 @@ public class BalanceServiceImpl implements BalanceService {
             balance.setAccountId(balance.getAccountId());
             balance.setUserId(balance.getUserId());
             BalanceEntity savedBalanced=balanceRepository.save(balance);
-            return ResponseBuilder.getSuccessResponse(HttpStatus.OK,savedBalanced,"Successfully Retrieved Balance");
+            return ResponseBuilder.getSuccessResponse(HttpStatus.OK,savedBalanced,
+                    "Successfully Retrieved Balance");
         }
         return ResponseBuilder.getFailResponse(HttpStatus.NO_CONTENT,null,
                 "No balance found");
