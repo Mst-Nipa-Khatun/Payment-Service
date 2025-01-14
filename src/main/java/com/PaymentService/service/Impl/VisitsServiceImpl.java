477f3a47ef49f1846032e69e57cf442f2673f63c
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-14 11:50:27 +0600

    "Added New API Visits"

--- src/main/java/com/PaymentService/service/Impl/VisitsServiceImpl.java
+++ src/main/java/com/PaymentService/service/Impl/VisitsServiceImpl.java
@@ -2,11 +2,19 @@ package com.PaymentService.service.Impl;
 
 import com.PaymentService.dto.Response;
 import com.PaymentService.dto.VisitsDto;
+import com.PaymentService.repository.VisitsRepository;
 import com.PaymentService.service.VisitsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VisitsServiceImpl implements VisitsService {
+    private final VisitsRepository visitsRepository;
+
+
+    public VisitsServiceImpl(VisitsRepository visitsRepository) {
+        this.visitsRepository = visitsRepository;
+    }
+
     @Override
     public Response createVisits(VisitsDto visitsDto) {
         return null;
