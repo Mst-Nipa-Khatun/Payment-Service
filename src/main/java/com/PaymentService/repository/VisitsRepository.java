98429f540556b2cfb0a2516a2319f8fa9b719277
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-14 12:16:31 +0600

    "Added New API Visits"

--- src/main/java/com/PaymentService/repository/VisitsRepository.java
+++ src/main/java/com/PaymentService/repository/VisitsRepository.java
@@ -1,9 +1,22 @@
 package com.PaymentService.repository;
 
+import com.PaymentService.dto.UserDetailsProjection;
+import com.PaymentService.dto.VisitsDetailsProjection;
 import com.PaymentService.entity.VisitsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VisitsRepository extends JpaRepository<VisitsEntity, Integer> {
+    VisitsEntity findByVisitId(Integer visitId);
+
+    @Query(value = "SELECT customer_id , COUNT(*) as count_no_trans\n" +
+            "FROM Visits\n" +
+            "WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)\n" +
+            "GROUP BY customer_id", nativeQuery = true)
+    List<VisitsDetailsProjection> findByVisitsAndNoTransaction();
+
 }
