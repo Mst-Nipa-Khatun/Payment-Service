a1fa3764d33f15f8c54c965e13eab2eb5dc1fa54
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-14 12:16:41 +0600

    "Added New API Visits"

--- src/main/java/com/PaymentService/service/Impl/VisitsServiceImpl.java
+++ src/main/java/com/PaymentService/service/Impl/VisitsServiceImpl.java
@@ -2,8 +2,11 @@ package com.PaymentService.service.Impl;
 
 import com.PaymentService.dto.Response;
 import com.PaymentService.dto.VisitsDto;
+import com.PaymentService.entity.VisitsEntity;
 import com.PaymentService.repository.VisitsRepository;
 import com.PaymentService.service.VisitsService;
+import com.PaymentService.utils.ResponseBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +20,15 @@ public class VisitsServiceImpl implements VisitsService {
 
     @Override
     public Response createVisits(VisitsDto visitsDto) {
-        return null;
+        VisitsEntity visits=visitsRepository.findByVisitId(visitsDto.getVisitId());
+        if(visits==null) {
+            visits=new VisitsEntity();
+            visits.setVisitId(visitsDto.getVisitId());
+            visits.setCustomerId(visitsDto.getCustomerId());
+           VisitsEntity savedVisits= visitsRepository.save(visits);
+           return ResponseBuilder.getSuccessResponse(HttpStatus.CREATED,savedVisits,"successfully created");
+
+        }
+        return ResponseBuilder.getFailResponse(HttpStatus.BAD_REQUEST,null,"Already exists");
     }
 }
