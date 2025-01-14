615b42cd97474a2b3ad18ca0a5aa2b3798adaf03
Mst-Nipa-Khatun <mknipa.iit@gmail.com>
2025-01-14 11:49:06 +0600

    "Added New API Visits"

---
+++ src/main/java/com/PaymentService/service/Impl/VisitsServiceImpl.java
@@ -0,0 +1,14 @@
+package com.PaymentService.service.Impl;
+
+import com.PaymentService.dto.Response;
+import com.PaymentService.dto.VisitsDto;
+import com.PaymentService.service.VisitsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitsServiceImpl implements VisitsService {
+    @Override
+    public Response createVisits(VisitsDto visitsDto) {
+        return null;
+    }
+}
